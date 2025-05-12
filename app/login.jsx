e02cde7fb93a315ae1ec24959244aba4e26a3355
Hossein Ansari <hosseinansari3@gmail.com>
2025-05-12 14:22:42 +0330

    create login page

---
+++ app/login.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text } from "react-native";
+import ScreenWrapper from "../components/ScreenWrapper";
+
+const Login = () => {
+  return (
+    <ScreenWrapper>
+      <Text>login</Text>
+    </ScreenWrapper>
+  );
+};
+
+export default Login;
+
+const styles = StyleSheet.create({});
