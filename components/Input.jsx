7309359c04230687f08d792a27f7245197b9607e
Hossein Ansari <hosseinansari3@gmail.com>
2025-05-13 22:07:23 +0330

    create text input component

---
+++ components/Input.jsx
@@ -0,0 +1,36 @@
+import { StyleSheet, TextInput, View } from "react-native";
+import { hp } from "../app/helpers/common";
+import { theme } from "../constants/theme";
+
+const Input = (props) => {
+  return (
+    <View
+      style={[styles.container, props.containerStyles && props.containerStyles]}
+    >
+      {props.icon && props.icon}
+      <TextInput
+        style={{ flex: 1 }}
+        placeholderTextColor={theme.colors.textLight}
+        ref={props.inputRef && props.inputRef}
+        {...props}
+      />
+    </View>
+  );
+};
+
+export default Input;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    height: hp(7.2),
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 0.4,
+    borderColor: theme.colors.text,
+    borderRadius: theme.radius.xxl,
+    borderCurve: "continuous",
+    paddingHorizontal: 18,
+    gap: 12,
+  },
+});
