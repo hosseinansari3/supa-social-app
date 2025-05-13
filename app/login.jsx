1cfd3237bea72e31b9736bdcae91c5020a05a3e2
Hossein Ansari <hosseinansari3@gmail.com>
2025-05-13 22:08:32 +0330

    add text and input to login page

--- app/login.jsx
+++ app/login.jsx
@@ -1,14 +1,51 @@
-import { StyleSheet, Text } from "react-native";
+import { useRouter } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Icon from "../assets/icons";
+import BackButton from "../components/BackButton";
+import Input from "../components/Input";
 import ScreenWrapper from "../components/ScreenWrapper";
+import { theme } from "../constants/theme";
+import { hp, wp } from "./helpers/common";
+
+const router = useRouter();
 
 const Login = () => {
   return (
     <ScreenWrapper>
-      <Text>login</Text>
+      <StatusBar style="dark" />
+      <View style={styles.container}>
+        <BackButton router={router} />
+
+        {/*Welcome*/}
+        <View>
+          <Text style={styles.welcomeText}>Hey,</Text>
+          <Text style={styles.welcomeText}>Welcome Back</Text>
+        </View>
+
+        {/*Form*/}
+        <View style={styles.form}>
+          <Text style={{ fontSize: hp(1.5), color: theme.colors.text }}>
+            Please login to continue
+          </Text>
+          <Input icon={<Icon name="mail" size="26" strokeWidth="1.6" />} />
+        </View>
+      </View>
     </ScreenWrapper>
   );
 };
 
 export default Login;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: 45,
+    paddingHorizontal: wp(5),
+  },
+  welcomeText: {
+    fontSize: hp(4),
+    fontWeight: theme.fonts.semibold,
+    color: theme.colors.text,
+  },
+});
