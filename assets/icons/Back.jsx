5de55300e1013b1df2e404cbe783664619a62193
Hossein Ansari <hosseinansari3@gmail.com>
2025-05-14 20:49:56 +0330

    add stroke width prop

--- assets/icons/Back.jsx
+++ assets/icons/Back.jsx
@@ -2,16 +2,16 @@ import Svg, { G, Path } from "react-native-svg";
 
 const Back = (props) => (
   <Svg
-    viewBox="0 0 1524 1024"
+    viewBox="0 0 1024 1024"
     width={24}
     height={24}
     fill="#000000"
     class="icon"
+    stroke="#000000"
     version="1.1"
-    xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
-    <G id="SVGRepo_bgCarrier" stroke-width={props.strokeWidth}></G>
+    <G id="SVGRepo_bgCarrier" stroke-width="0"></G>
     <G
       id="SVGRepo_tracerCarrier"
       stroke-linecap="round"
@@ -21,6 +21,7 @@ const Back = (props) => (
       <Path
         d="M669.6 849.6c8.8 8 22.4 7.2 30.4-1.6s7.2-22.4-1.6-30.4l-309.6-280c-8-7.2-8-17.6 0-24.8l309.6-270.4c8.8-8 9.6-21.6 2.4-30.4-8-8.8-21.6-9.6-30.4-2.4L360.8 480.8c-27.2 24-28 64-0.8 88.8l309.6 280z"
         fill=""
+        stroke-width={props.strokeWidth}
       ></Path>
     </G>
   </Svg>
