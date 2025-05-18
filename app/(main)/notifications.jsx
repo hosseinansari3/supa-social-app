037ca4372288e2e52f6cb6811e1a47d36529e9f7
Hossein Ansari <hosseinansari3@gmail.com>
2025-05-18 22:42:00 +0330

    create notifications initial page

---
+++ app/(main)/notifications.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const Notifications = () => {
+  return (
+    <View>
+      <Text>notifications</Text>
+    </View>
+  );
+};
+
+export default Notifications;
+
+const styles = StyleSheet.create({});
