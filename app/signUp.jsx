b1182f6d31990a64cc83ca64957642b20de79d8a
hosseinansari3 <hosseinansari3@gmail.com>
2025-05-18 21:34:29 +0330

    add signUp page ui elements and styles

--- app/signUp.jsx
+++ app/signUp.jsx
@@ -2,12 +2,94 @@ import { StyleSheet, Text, View } from "react-native";
 
 const SignUp = () => {
   return (
+    <ScreenWrapper>
+      <StatusBar style="dark" />
+      <View style={styles.container}>
+        <BackButton router={router} />
+
+        {/*Welcome*/}
     <View>
-      <Text>signUp</Text>
+          <Text style={styles.welcomeText}>Let's</Text>
+          <Text style={styles.welcomeText}>Get Started</Text>
+        </View>
+
+        {/*Form*/}
+        <View style={styles.form}>
+          <Text style={{ fontSize: hp(1.5), color: theme.colors.text }}>
+            Please fill the details to create a new account
+          </Text>
+          <Input
+            icon={<Icon name="user" size="26" strokeWidth="2.6" />}
+            placeholder="Enter your name"
+            onChangeText={(value) => (nameRef.current = value)}
+    />
+          <Input
+            icon={<Icon name="mail" size="26" strokeWidth="2.6" />}
+            placeholder="Enter your email"
+            onChangeText={(value) => (emailRef.current = value)}
+          />
+          <Input
+            icon={<Icon name="lock" size="26" strokeWidth="2.6" />}
+            placeholder="Enter your password"
+            onChangeText={(value) => (passwordRef.current = value)}
+            secureTextEntry
+          />
+          {/*button*/}
+          <Button title="Sign up" loading={loading} onPress={onSubmit} />
+        </View>
+
+        {/*footer*/}
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>Already have an account?</Text>
+          <Pressable onPress={() => router.push("login")}>
+            <Text
+              style={[
+                styles.footerText,
+                {
+                  color: theme.colors.primaryDark,
+                  fontWeight: theme.fonts.semibold,
+                },
+              ]}
+            >
+              Login
+            </Text>
+          </Pressable>
+        </View>
     </View>
+    </ScreenWrapper>
   );
 };
 
 export default SignUp;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: 45,
+    paddingHorizontal: wp(5),
+  },
+  welcomeText: {
+    fontSize: hp(4),
+    fontWeight: theme.fonts.semibold,
+    color: theme.colors.text,
+  },
+  form: {
+    gap: 25,
+  },
+  forgetPassword: {
+    textAlign: "right",
+    fontWeight: theme.fonts.semibold,
+    color: theme.colors.text,
+  },
+  footer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 5,
+  },
+  footerText: {
+    textAlign: "center",
+    color: theme.colors.text,
+    fontSize: hp(1.6),
+  },
+});
