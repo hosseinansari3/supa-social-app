e4a5be61a20758d229959158ae9c108af13d9ac8
Hossein Ansari <hosseinansari3@gmail.com>
2025-05-20 01:34:17 +0330

    create user profile header

--- app/(main)/profile.jsx
+++ app/(main)/profile.jsx
@@ -1,9 +1,25 @@
+import { useRouter } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
+import Header from "../../components/Header";
+import ScreenWrapper from "../../components/ScreenWrapper";
+import { useAuth } from "../contexts/authContext";
 
 const Profile = () => {
+  const { user, setAuth } = useAuth();
+  const router = useRouter();
   return (
-    <View>
+    <ScreenWrapper bg="white">
       <Text>profile</Text>
+    </ScreenWrapper>
+  );
+};
+
+const userHeader = ({ user, router }) => {
+  return (
+    <View style={{ flex: 1, backgroundColor: "white" }}>
+      <View>
+        <Header title="profile" showBackButton />
+      </View>
     </View>
   );
 };
