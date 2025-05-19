36163eec58cad80c37c74a07b5893f1e2385196a
Hossein Ansari <hosseinansari3@gmail.com>
2025-05-20 01:34:41 +0330

    create header component

---
+++ components/Header.jsx
@@ -0,0 +1,40 @@
+import { useRouter } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+import { hp } from "../app/helpers/common";
+import { theme } from "../constants/theme";
+import BackButton from "./BackButton";
+
+const Header = ({ title, showBackButton = false, mb = 10 }) => {
+  const router = useRouter();
+  return (
+    <View style={[styles.container, { marginBottom: mb }]}>
+      {showBackButton && (
+        <View style={styles.backButton}>
+          <BackButton router={router} />
+        </View>
+      )}
+      <Text style={styles.title}>{title || ""}</Text>
+    </View>
+  );
+};
+
+export default Header;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 5,
+    gap: 10,
+  },
+  title: {
+    fontSize: hp(2.7),
+    fontWeight: theme.fonts.semibold,
+    color: theme.colors.textDark,
+  },
+  backButton: {
+    position: "absolute",
+    left: 0,
+  },
+});
