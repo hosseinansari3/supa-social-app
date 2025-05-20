78f6dff498198f4f023de8cd8e5651f4b358c7c9
hosseinansari3 <hosseinansari3@gmail.com>
2025-05-20 17:02:30 +0330

    add styles and imports

--- app/(main)/profile.jsx
+++ app/(main)/profile.jsx
@@ -1,8 +1,19 @@
 import { useRouter } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Alert,
+  Pressable,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import Icon from "../../assets/icons";
+import Avatar from "../../components/Avatar";
 import Header from "../../components/Header";
 import ScreenWrapper from "../../components/ScreenWrapper";
+import { theme } from "../../constants/theme";
+import { supabase } from "../../lib/supabase";
 import { useAuth } from "../contexts/authContext";
+import { hp, wp } from "../helpers/common";
 
 const Profile = () => {
   const { user, setAuth } = useAuth();
@@ -33,14 +44,16 @@ const Profile = () => {
   };
   return (
     <ScreenWrapper bg="white">
-      <Text>profile</Text>
+      <UserHeader user={user} router={router} handleLogout={handleLogout} />
     </ScreenWrapper>
   );
 };
 
-const userHeader = ({ user, router }) => {
+const UserHeader = ({ user, router, handleLogout }) => {
   return (
-    <View style={{ flex: 1, backgroundColor: "white" }}>
+    <View
+      style={{ flex: 1, backgroundColor: "white", paddingHorizontal: wp(4) }}
+    >
       <View>
         <Header title="profile" showBackButton={true} />
         <TouchableOpacity style={styles.logoutButton} onPress={handleLogout}>
@@ -68,4 +81,41 @@ const userHeader = ({ user, router }) => {
 
 export default Profile;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  headerContainer: {
+    marginHorizontal: wp(4),
+    marginBottom: 20,
+  },
+  headerShape: {
+    width: hp(100),
+    height: hp(20),
+  },
+  avatarContainer: {
+    height: hp(12),
+    width: hp(12),
+    alignSelf: "center",
+  },
+  logoutButton: {
+    position: "absolute",
+    right: 0,
+    padding: 5,
+    borderRadius: theme.radius.sm,
+    backgroundColor: "#fee2e2",
+  },
+  editeIcon: {
+    position: "absolute",
+    bottom: 0,
+    right: -12,
+    padding: 7,
+    borderRadius: 50,
+    backgroundColor: "white",
+    shadowColor: theme.colors.textLight,
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.4,
+    shadowRadius: 5,
+    elevation: 7,
+  },
+});
