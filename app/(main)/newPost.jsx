abeed79f54e86733b35ef51e00c73a42ed3d17a0
Hossein Ansari <hosseinansari3@gmail.com>
2025-05-27 17:58:39 +0330

    feat: create header of new post page

--- app/(main)/newPost.jsx
+++ app/(main)/newPost.jsx
@@ -1,13 +1,25 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import Header from "../../components/Header";
+import ScreenWrapper from "../../components/ScreenWrapper";
 
 const NewPost = () => {
   return (
-    <View>
-      <Text>newPost</Text>
-    </View>
+    <ScreenWrapper bg="white">
+      <View style={styles.container}>
+        <Header title="Create post" />
+      </View>
+    </ScreenWrapper>
   );
 };
 
 export default NewPost;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // backgroundColor: 'red',
+    marginBottom: 30,
+    paddingHorizontal: wp(4),
+    gap: 15,
+  },
+});
