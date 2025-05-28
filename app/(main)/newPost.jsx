e5686814aeaa76f2adbcae2dc7afe155ec1c996f
Hossein Ansari <hosseinansari3@gmail.com>
2025-05-28 14:04:00 +0330

    feat: add user avatar

--- app/(main)/newPost.jsx
+++ app/(main)/newPost.jsx
@@ -1,12 +1,28 @@
 import { StyleSheet, View } from "react-native";
+import Avatar from "../../components/Avatar";
 import Header from "../../components/Header";
 import ScreenWrapper from "../../components/ScreenWrapper";
+import { theme } from "../../constants/theme";
+import { useAuth } from "../contexts/authContext";
+import { hp } from "../helpers/common";
 
 const NewPost = () => {
+  const { user } = useAuth();
   return (
     <ScreenWrapper bg="white">
       <View style={styles.container}>
         <Header title="Create post" />
+        <ScrollView contentContainerStyle={{ gap: 20 }}>
+          {/* avatar */}
+
+          <View style={styles.header}>
+            <Avatar
+              uri={user?.image}
+              size={hp(6.5)}
+              rounded={theme.radius.xl}
+            />
+          </View>
+        </ScrollView>
       </View>
     </ScreenWrapper>
   );
@@ -21,5 +37,10 @@ const styles = StyleSheet.create({
     marginBottom: 30,
     paddingHorizontal: wp(4),
     gap: 15,
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 12,
   },
 });
