c37410a7c7e95f77c012fc0051397c8cd8293eac
hosseinansari3 <hosseinansari3@gmail.com>
2025-05-28 16:49:50 +0330

    feat: create rich text editor component

---
+++ components/RichTextEditor.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const RichTextEditor = ({ editorRef, onChange }) => {
+  return (
+    <View>
+      <Text>RichTextEditor</Text>
+    </View>
+  );
+};
+
+export default RichTextEditor;
+
+const styles = StyleSheet.create({});
