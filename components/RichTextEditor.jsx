ffece09d6ea1e5d6226fc98152e10fddf2146d6d
hosseinansari3 <hosseinansari3@gmail.com>
2025-05-29 15:42:42 +0330

    feat: create and initialze rich toolbar

--- components/RichTextEditor.jsx
+++ components/RichTextEditor.jsx
@@ -1,9 +1,28 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { RichToolbar } from "react-native-pell-rich-editor";
 
 const RichTextEditor = ({ editorRef, onChange }) => {
   return (
-    <View>
-      <Text>RichTextEditor</Text>
+    <View style={{ minHeight: 285 }}>
+      <RichToolbar
+        actions={[
+          actions.setStrikethrough,
+          actions.removeFormat,
+          actions.setBold,
+          actions.setItalic,
+          actions.insertOrderedList,
+          actions.blockquote,
+          actions.alignLeft,
+          actions.alignCenter,
+          actions.alignRight,
+          actions.code,
+          actions.line,
+        ]}
+        style={styles.richBar}
+        flatContainerStyle={styles.listStyle}
+        editor={editorRef}
+        disabled={false}
+      />
     </View>
   );
 };
