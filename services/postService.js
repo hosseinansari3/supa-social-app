fffdeb1cb8bb76c05be75af540a81cbb0ae24b14
Hossein Ansari <hosseinansari3@gmail.com>
2025-06-02 15:41:42 +0330

    feat: create method for fetching posts

--- services/postService.js
+++ services/postService.js
@@ -30,3 +30,23 @@ export const createOrUpadtePost = async (post) => {
     return { success: false, msg: "could not create your post" };
   }
 };
+
+export const fetchPosts = async (limit = 10) => {
+  try {
+    const { data, error } = await supabase
+      .from("posts")
+      .select(`*, user: users (id,  name, image)`)
+      .order("created_at", { ascending: false })
+      .limit(limit);
+
+    if (error) {
+      console.log("fetch post error", error);
+      return { success: false, msg: "could not fetch the posts" };
+    }
+
+    return { success: true, data: data };
+  } catch (error) {
+    console.log("fetch post error", error);
+    return { success: false, msg: "could not fetch the posts" };
+  }
+};
