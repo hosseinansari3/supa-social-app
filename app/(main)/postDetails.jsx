f4d56f39f7b6f1f0a30793bc104f9f3e74ca6055
Hossein Ansari <hosseinansari3@gmail.com>
2025-06-07 15:26:57 +0330

    feat: close modal by swiping down

--- app/(main)/postDetails.jsx
+++ app/(main)/postDetails.jsx
@@ -1,9 +1,48 @@
 import { useLocalSearchParams, useRouter } from "expo-router";
+import { StyleSheet, Text } from "react-native";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
+import Animated, {
+  runOnJS,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+  withTiming,
+} from "react-native-reanimated";
+import ScreenWrapper from "../../components/ScreenWrapper";
+import { theme } from "../../constants/theme";
+import { hp, wp } from "../helpers/common";
+
 const PostDetails = () => {
   const { postId } = useLocalSearchParams();
   console.log("got post id", postId);
+
+  const router = useRouter();
+
+  const translateY = useSharedValue(0);
+
+  const panGesture = Gesture.Pan()
+    .onUpdate((e) => {
+      if (e.translationY > 0) {
+        translateY.value = e.translationY;
+      }
+    })
+    .onEnd(() => {
+      if (translateY.value > 200) {
+        translateY.value = withTiming(1000, { duration: 250 }, (finished) => {
+          if (finished) runOnJS(router.back)();
+        });
+      } else {
+        translateY.value = withSpring(0); // snap back
+      }
+    });
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ translateY: translateY.value }],
+  }));
+
   return (
     <ScreenWrapper>
+      <GestureDetector gesture={panGesture}>
         <Animated.View
           style={[
             {
@@ -26,6 +65,7 @@ const PostDetails = () => {
         >
           <Text>postDetails</Text>
         </Animated.View>
+      </GestureDetector>
     </ScreenWrapper>
   );
 };
