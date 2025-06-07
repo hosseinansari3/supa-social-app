79fa5008a33a5be5e242db2e632afd6555ccdc46
Hossein Ansari <hosseinansari3@gmail.com>
2025-06-07 15:24:41 +0330

    feat: receive search params

--- app/(main)/postDetails.jsx
+++ app/(main)/postDetails.jsx
@@ -1,4 +1,7 @@
+import { useLocalSearchParams, useRouter } from "expo-router";
 const PostDetails = () => {
+  const { postId } = useLocalSearchParams();
+  console.log("got post id", postId);
   return (
     <ScreenWrapper>
     </ScreenWrapper>
