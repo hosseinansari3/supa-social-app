89c9a13dac0face22300a08fe1a2721e7e4d3b4a
Hossein Ansari <hosseinansari3@gmail.com>
2025-06-07 15:26:26 +0330

    feat: add styles for container

--- app/(main)/postDetails.jsx
+++ app/(main)/postDetails.jsx
@@ -4,6 +4,28 @@ const PostDetails = () => {
   console.log("got post id", postId);
   return (
     <ScreenWrapper>
+        <Animated.View
+          style={[
+            {
+              marginTop: 20,
+              backgroundColor: "white",
+              height: hp(100),
+              borderRadius: theme.radius.md,
+              width: wp(99),
+              alignSelf: "center",
+              shadowOffset: {
+                width: 0,
+                height: 2,
+              },
+              shadowOpacity: 0.06,
+              shadowRadious: 6,
+              elevation: 1,
+            },
+            animatedStyle,
+          ]}
+        >
+          <Text>postDetails</Text>
+        </Animated.View>
     </ScreenWrapper>
   );
 };
