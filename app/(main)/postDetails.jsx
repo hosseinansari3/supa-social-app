19a96dc9c46d22bf0d159332a5e11553dcfba169
Hossein Ansari <hosseinansari3@gmail.com>
2025-06-08 22:40:03 +0330

    feat: show comment list

--- app/(main)/postDetails.jsx
+++ app/(main)/postDetails.jsx
@@ -155,6 +155,28 @@ const PostDetails = () => {
                   </TouchableOpacity>
                 )}
               </View>
+
+              {/* comment list */}
+
+              <View style={{ marginVertical: 15, gap: 17 }}>
+                {post?.comments?.map((comment) => (
+                  <CommentItem
+                    key={comment?.id?.toString()}
+                    item={comment}
+                    canDelete={
+                      user.id == comment.userId || user?.id == post?.userId
+                    }
+                  />
+                ))}
+
+                {post?.comments?.length == 0 && (
+                  <Text style={{ color: theme.colors.text, marginLeft: 5 }}>
+                    Be first to comment!
+                  </Text>
+                )}
+              </View>
+            </ScrollView>
+          )}
         </Animated.View>
       </GestureDetector>
     </ScreenWrapper>
