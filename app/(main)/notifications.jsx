b597def6c675affaeb766819a35100594acb0db3
Hossein Ansari <hosseinansari3@gmail.com>
2025-07-12 00:24:19 +0330

    feat: create notifications screen

--- app/(main)/notifications.jsx
+++ app/(main)/notifications.jsx
@@ -1,13 +1,65 @@
-import { StyleSheet, Text, View } from "react-native";
+import { useRouter } from "expo-router";
+import { useEffect, useState } from "react";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import Header from "../../components/Header";
+import NotificationItem from "../../components/NotificationItem";
+import ScreenWrapper from "../../components/ScreenWrapper";
+import { theme } from "../../constants/theme";
+import { fetchNotification } from "../../services/notificationService";
+import { useAuth } from "../contexts/authContext";
+import { hp, wp } from "../helpers/common";
 
 const Notifications = () => {
+  const [notifications, setNotifications] = useState([]);
+  const { user } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    getNotifications();
+  }, []);
+
+  const getNotifications = async () => {
+    let res = await fetchNotification(user.id);
+    if (res.success) setNotifications(res.data);
+  };
+
   return (
-    <View>
-      <Text>notifications</Text>
-    </View>
+    <ScreenWrapper>
+      <View style={styles.container}>
+        <Header title="Notifications" />
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={styles.listStyle}
+        >
+          {notifications.map((item) => {
+            return (
+              <NotificationItem item={item} key={item?.id} router={router} />
+            );
+          })}
+          {notifications.length == 0 && (
+            <Text style={styles.nodata}>no notifications yet</Text>
+          )}
+        </ScrollView>
+      </View>
+    </ScreenWrapper>
   );
 };
 
 export default Notifications;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: wp(4),
+  },
+  listStyle: {
+    paddingVertical: 20,
+    gap: 10,
+  },
+  nodata: {
+    fontSize: hp(1.8),
+    fontWeight: theme.fonts.medium,
+    color: theme.colors.text,
+    textAlign: "center",
+  },
+});
