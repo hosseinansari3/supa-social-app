0acbb0904005fa42e781a7919bab36d98de997ea
Hossein Ansari <hosseinansari3@gmail.com>
2025-07-12 00:21:40 +0330

    feat: build notifications fetching service

--- services/notificationService.js
+++ services/notificationService.js
@@ -20,3 +20,25 @@ export const createNotification = async (notification) => {
   }
 };
 
+export const fetchNotification = async (receiverId) => {
+  try {
+    const { data, error } = await supabase
+      .from("notifications")
+      .select(
+        `*, sender: senderId(id, name, image)
+      `
+      )
+      .eq("receiverId", receiverId)
+      .order("created_at", { ascending: false });
+
+    if (error) {
+      console.log("fetchNotification error", error);
+      return { success: false, msg: "could not fetch Notifications" };
+    }
+
+    return { success: true, data: data };
+  } catch (error) {
+    console.log("fetchNotification error", error);
+    return { success: false, msg: "could not fetch Notifications" };
+  }
+};
