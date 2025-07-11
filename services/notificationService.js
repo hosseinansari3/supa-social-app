f1e54485be7e7a9806904eb5df87168eef6ebc87
Hossein Ansari <hosseinansari3@gmail.com>
2025-07-12 00:20:59 +0330

    feat: build notification creating service

---
+++ services/notificationService.js
@@ -0,0 +1,22 @@
+import { supabase } from "../lib/supabase";
+
+export const createNotification = async (notification) => {
+  try {
+    const { data, error } = await supabase
+      .from("notifications")
+      .insert(notification)
+      .select()
+      .single();
+
+    if (error) {
+      console.log("notification error", error);
+      return { success: false, msg: "something went wrong" };
+    }
+
+    return { success: true, data: data };
+  } catch (error) {
+    console.log("postLike error", error);
+    return { success: false, msg: "something went wrong" };
+  }
+};
+
