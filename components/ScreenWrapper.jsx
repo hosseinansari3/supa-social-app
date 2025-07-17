f757db2faf75eb8a74f2a9ce6270089ba71753a9
Hossein Ansari <hosseinansari3@gmail.com>
2025-07-17 15:57:12 +0330

    feat: use safe area for padding bottom

--- components/ScreenWrapper.jsx
+++ components/ScreenWrapper.jsx
@@ -2,10 +2,19 @@ import { View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const ScreenWrapper = ({ children, bg }) => {
-  const { top } = useSafeAreaInsets();
+  const { top, bottom } = useSafeAreaInsets();
   const paddingTop = top > 0 ? top + 5 : 30;
   return (
-    <View style={{ flex: 1, paddingTop, backgroundColor: bg }}>{children}</View>
+    <View
+      style={{
+        flex: 1,
+        paddingTop,
+        paddingBottom: bottom,
+        backgroundColor: bg,
+      }}
+    >
+      {children}
+    </View>
   );
 };
 
